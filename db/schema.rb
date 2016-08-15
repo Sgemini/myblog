d49639f205dc400f2673808dba2e8e90ec077c55
vincent <vliang@xogrp.com>
2016-08-15 14:43:47 +0800

    add git ignore

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160309115938) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,4 +79,5 @@ ActiveRecord::Schema.define(version: 20160309115938) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
   end
+
 end
