4f75426be651c200f480a1a56d161e27cf9e800b
vliang <vliang@xogrp.com>
2017-07-24 15:39:44 +0800

    done

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,7 +3,10 @@
     <h2>最新文章</h2>
     <ul>
       <li v-for='article in articles'>
-        <a :href="'#/articles/' + article.title">{{ article.title }}</a>
+        <router-link :to="{ path: '/articles/'+article.title }">
+          {{ article.title }}
+        </router-link>
+        <time id='article-time'>{{ dateFormat(article.created_at) }}</time>
       </li>
     </ul>
     <a class='more' href='#/articles'>更多</a>
@@ -11,21 +14,19 @@
 </template>
 
 <script>
+  import mixin from '@/share/share'
   export default {
+    mixins: [mixin],
     data () {
       return {
-        articles: [
-          { title: 'article1' },
-          { title: 'article2' },
-          { title: 'article3' },
-          { title: 'article4' },
-          { title: 'article5' }
-        ]
+        articles: []
       }
+    },
+    created () {
+      this.$http.get('http://localhost:3000/articles').then(response => {
+        this.$set(this, 'articles', response.body)
+      })
     }
-    // created () {
-    //   this.fetchData()
-    // },
   }
 </script>
 
@@ -34,6 +35,9 @@
   .more {
     display: block;
     text-align: right;
+  }
+  #article-time {
+    float: right;
   }
   #articles-index {
     padding: 20px;
